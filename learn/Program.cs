aa804451faa56a18a42a7b5f566a552f6f65f8ac
crazyWander <dmtrern@gmail.com>
2023-03-25 23:19:45 +0400

    update readME.md

--- learn/Program.cs
+++ learn/Program.cs
@@ -1,13 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using learn;
 
 internal class Program
 {
     public static void Main(string[] args)
     {
-        Viselica Game = new Viselica("привет");
-        Game.StartGame();
-        //Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World!");
     }
 }

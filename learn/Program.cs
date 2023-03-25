bfddac7e30bb949c07bd649b6cded3e3dfea2567
crazyWander <dmtrern@gmail.com>
2023-03-25 20:07:02 +0400

    init

---
+++ learn/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using learn;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Viselica Game = new Viselica("привет");
+        Game.StartGame();
+        //Console.WriteLine("Hello, World!");
+    }
+}
